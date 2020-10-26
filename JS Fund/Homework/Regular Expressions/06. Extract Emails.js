5908f341e33b2b4d8b13fec9141e41f6146e56a1
MapoBG <mapobg@gmail.com>
2020-10-26 17:52:46 +0200

    .

--- JS Fund/Homework/Regular Expressions/06. Extract Emails.js
+++ JS Fund/Homework/Regular Expressions/06. Extract Emails.js
@@ -2,8 +2,6 @@ function getEmails(input) {
     let validEmails = input[0].match(/(^|(?<=\s))(([a-zA-Z0-9]+)([\.\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\.\-_][A-Za-z]+)+))(\b|(?=\s))/g);
     if (validEmails) {
         console.log(validEmails.join("\n"));
-    } else {
-        validEmails = [];
-    }
+    } 
 }
 getEmails(['Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.'])
