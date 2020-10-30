f5a8e8ebe3d7b70331577ce6b1d0135487d81867
MapoBG <mapobg@gmail.com>
2020-10-30 12:02:31 +0200

    .

--- JS Advanced/Advanced Functions/Excercise/01. Sort Array.js
+++ JS Advanced/Advanced Functions/Excercise/01. Sort Array.js
@@ -1,11 +1,11 @@
 function sortArray(arr, sortType) {
-    let sorted = sortType == 'asc' ? arr.sort((a, b) => a - b) : arr.sort((a, b) => b - a);
-    return sorted;
+    // let sorted = sortType == 'asc' ? arr.sort((a, b) => a - b) : arr.sort((a, b) => b - a);
+    // return sorted;
 
-    // let sort = {
-    //     'asc': (arr) => arr.sort((a, b) => a - b),
-    //     'desc': (arr) => arr.sort((a, b) => b - a),
-    // };
-    // return (obj[sortType](arr));
+    let sort = {
+        'asc': (arr) => arr.sort((a, b) => a - b),
+        'desc': (arr) => arr.sort((a, b) => b - a),
+    };
+    return (sort[sortType](arr));
 }
 sortArray([14, 7, 17, 6, 8], 'desc');
--- JS Advanced/Function Context/Excercise/05. Next Article/next-article.js
+++ JS Advanced/Function Context/Excercise/05. Next Article/next-article.js
@@ -1,16 +1,15 @@
 function getArticleGenerator(articles) {
     let mainDiv = document.getElementById("content");
-    console.log(articles);
 
     function getNextArticle() {
         if (articles.length > 0) {
             let nextArticle = articles.shift();
             let divEl = document.createElement("article");
+            
             divEl.textContent = nextArticle;
             mainDiv.appendChild(divEl);
-
-            return nextArticle;
         }
     }
+
     return getNextArticle;
 }
