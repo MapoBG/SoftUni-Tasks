82442ecd3e0b68978fbde56d5ebc965d239528c5
MapoBG <mapobg@gmail.com>
2022-06-15 16:46:20 +0300

    added not found route

--- JS Web/2022/Back-end/ExpressJS-and-Templating-Workshop/src/routes.js
+++ JS Web/2022/Back-end/ExpressJS-and-Templating-Workshop/src/routes.js
@@ -9,5 +9,8 @@ router.use("/", homeRouter);
 router.use("/cubes", cubeRouter);
 router.use("/accessories", accRouter);
 router.use("/user", userRouter);
+router.use("*", (req, res) => {
+    res.render("404");
+});
 
 module.exports = router;
