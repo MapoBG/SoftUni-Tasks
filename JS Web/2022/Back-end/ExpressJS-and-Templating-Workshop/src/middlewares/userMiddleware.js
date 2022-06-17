ab8e30ff80ff72bb3bc0ce2e56b1473f0e5ef236
MapoBG <mapobg@gmail.com>
2022-06-17 15:41:56 +0300

    added authentication in the navigation

--- JS Web/2022/Back-end/ExpressJS-and-Templating-Workshop/src/middlewares/userMiddleware.js
+++ JS Web/2022/Back-end/ExpressJS-and-Templating-Workshop/src/middlewares/userMiddleware.js
@@ -9,7 +9,7 @@ exports.auth = (req, res, next) => {
             const decodedToken = jwt.verify(token, secret);
 
             req.user = decodedToken;
-            res.locals.user = decodedToken;
+            res.locals.user = decodedToken; //attach user to app context = can be accessed in hbs with {{user}}
         } catch (error) {
             return res.redirect("/404");
         }
