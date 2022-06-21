1825c6901038fbb059ec30cf7441440c32cb120c
MapoBG <mapobg@gmail.com>
2022-06-21 17:55:16 +0300

    added User model

---
+++ JS Web/2022/Back-end/Exam-prep/art-task/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userShema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    myPublications: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Publication'
+    }
+});
+
+module.exports = User = mongoose.model('User', userShema);
