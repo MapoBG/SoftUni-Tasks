1a2218ac8390c6f9ce5802c624064ec769ac0d92
MapoBG <mapobg@gmail.com>
2022-06-22 12:53:00 +0300

    added hash pwrd & reset values funcs

--- JS Web/2022/Back-end/Exam-prep/art-task/services/utils.js
+++ JS Web/2022/Back-end/Exam-prep/art-task/services/utils.js
@@ -1,8 +1,21 @@
 const jwt = require('jsonwebtoken');
-const { secret } = require('../config/env');
+const bcrypt = require('bcrypt');
 
-exports.createToken = (newUser) => {
+const { secret, saltRounds } = require('../config/env');
+
+exports.createToken = async (newUser) => {
     const token = jwt.sign({ _id: newUser._id, username: newUser.username }, secret, { expiresIn: '1d' });     //this is sync func by design...
 
     return token;
 };
+
+exports.hashPassword = (password) => {
+    const hashedPassword = bcrypt.hash(password, saltRounds);
+
+    return hashedPassword;
+};
+
+exports.resetValues = (result) => {
+    result.isValid = true;
+    result.msgs = [];
+}
