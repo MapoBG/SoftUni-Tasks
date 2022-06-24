cf1ee6013479a55e94f75581e07f179910281244
MapoBG <mapobg@gmail.com>
2022-06-24 11:36:29 +0300

    added create publication validations & err messages

--- JS Web/2022/Back-end/Exam-prep/art-task/controllers/publicationController.js
+++ JS Web/2022/Back-end/Exam-prep/art-task/controllers/publicationController.js
@@ -3,14 +3,18 @@ const publicationRouter = require('express').Router();
 const { isAuth } = require('../middlewares/userMiddleware');
 const { createPublication } = require('../services/-publicationServices-');
 
+publicationRouter.use(isAuth);
+
 publicationRouter.get('/create', (req, res) => res.render('-publications-/create'));
 
-publicationRouter.post('/create', isAuth, (req, res) => {
+publicationRouter.post('/create', async (req, res) => {
     const publicationData = req.body;
 
     try {
-        const newPublication = createPublication(publicationData)
+        const newPublication = await createPublication({ ...publicationData, author: req.user._id });
     } catch (error) {
+        res.locals.errors = [error.message];
+
         res.render('-publications-/create', { publicationData });
     }
 });
--- JS Web/2022/Back-end/Exam-prep/art-task/models/Publication.js
+++ JS Web/2022/Back-end/Exam-prep/art-task/models/Publication.js
@@ -3,23 +3,26 @@ const mongoose = require('mongoose');
 const publicationSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true,
+        required: [true, 'Title is required and should be at least 6 characters long'],
         minLength: [6, 'Title should be at least 6 characters long']
     },
     paintingTechnique: {
         type: String,
-        required: true,
+        required: [true, '"Painting technique" is required and should be no more than 15 characters long'],
         maxLength: [15, 'Painting technique should be max 15 characters long']
     },
     picture: {
         type: String,
-        required: true,
+        required: [true, 'Picture link is required'],
         match: /^https?:\/\//
     },
     certificate: {
         type: String,
-        required: true,
-        enum: ['Yes', 'No']       //default values
+        required: [true, 'Certificate is required'],
+        enum: {                      //default values
+            values: ['Yes', 'No'],
+            message: 'Certificate should be either "Yes" or "No"'
+        }
     },
     author: {
         type: mongoose.Types.ObjectId,
