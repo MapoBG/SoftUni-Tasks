49e7a2b63167f288f9f6c200321cce3f3438af3d
MapoBG <mapobg@gmail.com>
2022-07-08 15:38:27 +0300

    added no users error

--- JS Web/2022/React/Workshop/client/src/components/main/user-table/table-row/TableRow.js
+++ JS Web/2022/React/Workshop/client/src/components/main/user-table/table-row/TableRow.js
@@ -1,15 +1,17 @@
-export const TableRow = () => {
+export const TableRow = ({ user }) => {
+    const img = user.imageUrl || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+
     return (
         <tr>
             <td>
-                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+                <img src={img}
                     alt="Peter's profile" className="image" />
             </td>
-            <td>Peter</td>
-            <td>Johnson</td>
-            <td>peter@abv.bg</td>
-            <td>0812345678</td>
-            <td>June 28, 2022</td>
+            <td>{user.firstName}</td>
+            <td>{user.lastName}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+            <td>{user.dateCreated}</td>
 
             <td className="actions">
                 <button className="btn edit-btn" title="Edit">
