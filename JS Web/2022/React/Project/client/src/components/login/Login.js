d745382c70107c3c49658347b72c7652b2b4b30e
MapoBG <mapobg@gmail.com>
2022-08-01 16:45:26 +0300

    added controlled forms to login & register

--- JS Web/2022/React/Project/client/src/components/login/Login.js
+++ JS Web/2022/React/Project/client/src/components/login/Login.js
@@ -1,22 +1,39 @@
+import { useState } from "react";
+
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export const Login = () => {
+    const [loginData, setLoginData] = useState({
+        formBasicEmail: '',
+        formBasicPassword: ''
+    });
+
+    const loginDataHandler = (e) => {
+        setLoginData(oldState => ({ ...oldState, [e.target.id]: e.target.value }));
+    }
+
+    const loginHandler = (e) => {
+        e.preventDefault();
+
+        console.log(loginData);
+    }
+
     return (
-        <Form className="AuthForm">
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form className="AuthForm" onSubmit={(e) => loginHandler(e)} >
+            <Form.Group className="mb-3" controlId="formBasicEmail" onChange={(e) => loginDataHandler(e)}>
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3" controlId="formBasicPassword" onChange={(e) => loginDataHandler(e)}>
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
             </Form.Group>
-            
+
             <Button variant="primary" type="submit">
                 Submit
             </Button>
-        </Form>
+        </Form >
     );
 };
--- JS Web/2022/React/Project/client/src/components/register/Register.js
+++ JS Web/2022/React/Project/client/src/components/register/Register.js
@@ -1,24 +1,41 @@
+import { useState } from "react";
+
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export const Register = () => {
+    const [registerData, setRegisterData] = useState({
+        formBasicEmail: '',
+        formBasicPassword: '',
+        formBasicRePassword: ''
+    });
+
+    const registerDataHandler = (e) => {
+        setRegisterData(oldState => ({ ...oldState, [e.target.id]: e.target.value }));
+    }
+
+    const registerHandler = (e) => {
+        e.preventDefault();
+
+        console.log(registerData);
+    }
     return (
-        <Form className="AuthForm">
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form className="AuthForm" onSubmit={(e) => registerHandler(e)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail" onChange={(e) => registerDataHandler(e)}>
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3" controlId="formBasicPassword" onChange={(e) => registerDataHandler(e)}>
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicRePassword">
+            <Form.Group className="mb-3" controlId="formBasicRePassword" onChange={(e) => registerDataHandler(e)}>
                 <Form.Label>Repeat password</Form.Label>
                 <Form.Control type="password" placeholder="Repeat password" />
             </Form.Group>
-            
+
             <Button variant="primary" type="submit">
                 Submit
             </Button>
