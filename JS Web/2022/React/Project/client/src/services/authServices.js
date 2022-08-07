5e3f06f8f0c7bffa4cd6620db272deecc2517dd1
MapoBG <mapobg@gmail.com>
2022-08-07 19:18:49 +0300

    moved to separate file

---
+++ JS Web/2022/React/Project/client/src/services/authServices.js
@@ -0,0 +1,17 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth"
+import { firebaseAuth } from "../api/firebase"
+
+export const registerUser = async (userData) => {
+    const response = await createUserWithEmailAndPassword(firebaseAuth, userData.email, userData.password);
+    return response;
+};
+
+export const loginUser = async (userData) => {
+    const response = await signInWithEmailAndPassword(firebaseAuth, userData.email, userData.password);
+    return response;
+};
+
+export const logoutUser = async () => {
+    const response = await signOut(firebaseAuth);
+    return response;
+};
